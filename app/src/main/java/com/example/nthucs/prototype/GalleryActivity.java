961162ed35f2f984d31d69030b31f8c339b38885
NTHUCS <bouzinlp@gmail.com>
2016-07-01 14:45:37 +0800

    Gallery

--- app/src/main/java/com/example/nthucs/prototype/GalleryActivity.java
+++ app/src/main/java/com/example/nthucs/prototype/GalleryActivity.java
@@ -1,19 +1,31 @@
 package com.example.nthucs.prototype;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by NTHUCS on 2016/7/1.
  */
 
 public class GalleryActivity extends AppCompatActivity {
+
+    // 讀取外部儲存設備授權請求代碼
+    private static final int REQUEST_READ_EXTERNAL_STORAGE_PERMISSION = 101;
+    private static final int SELECT_FILE = 1;
 
     // Picture's original name and image view
     private String fileName;
@@ -41,16 +53,101 @@ public class GalleryActivity extends AppCompatActivity {
         // 取得顯示照片的ImageView元件
         picture = (ImageView) findViewById(R.id.picture);
 
+        food = new Food(resultText, fileName);
+
         if (action.equals("com.example.nthucs.prototype.TAKE_PHOTO")) {
             // new food
-            food = new Food(resultText, fileName);
+            requestStoragePermission();
         }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == Activity.RESULT_OK) {
-
+            if (requestCode == SELECT_FILE) {
+                onSelectFromGalleryResult(data);
+            }
         }
+    }
+
+    // 覆寫請求授權後執行的方法
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == REQUEST_READ_EXTERNAL_STORAGE_PERMISSION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                galleryIntent();
+            } else {
+                Toast.makeText(this, R.string.read_external_storage_denied,
+                        Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        File file = configFileName("P", ".jpg");
+        picFile = file;
+
+        if (file.exists()) {
+            // 顯示照片元件
+            //picture.setVisibility(View.VISIBLE);
+            // 設定照片
+            //FileUtil.fileToImageView(file.getAbsolutePath(), picture);
+        }
+    }
+
+    public void onSubmit(View view) {
+
+    }
+
+    private void requestStoragePermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            int hasPermission = checkSelfPermission(
+                    Manifest.permission.READ_EXTERNAL_STORAGE);
+
+            if (hasPermission != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        REQUEST_READ_EXTERNAL_STORAGE_PERMISSION);
+                return;
+            }
+        }
+
+        galleryIntent();
+    }
+
+    private void galleryIntent() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+
+        startActivityForResult(Intent.createChooser(intent, "Select File"), SELECT_FILE);
+    }
+
+    private File configFileName(String prefix, String extension) {
+        if (fileName == null) {
+            fileName = FileUtil.getUniqueFileName();
+        }
+
+        return new File(FileUtil.getExternalStorageDir(FileUtil.APP_DIR),
+                prefix + fileName + extension);
+    }
+
+    private void onSelectFromGalleryResult(Intent data) {
+        Bitmap bitmap =null;
+        if (data != null) {
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), data.getData());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        picture.setImageBitmap(bitmap);
+        picture.setVisibility(View.VISIBLE);
     }
 }
