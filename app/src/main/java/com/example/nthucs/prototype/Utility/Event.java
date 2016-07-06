fd8769650b4592754f334c3fe280137d8ec9eb68
NTHUCS <sirs2233@gmail.com>
2016-07-06 10:50:27 +0800

    add new class

--- app/src/main/java/com/example/nthucs/prototype/Utility/Event.java
+++ app/src/main/java/com/example/nthucs/prototype/Utility/Event.java
@@ -3,9 +3,5 @@ package com.example.nthucs.prototype.Utility;
 /**
  * Created by NTHUCS on 2016/7/6.
  */
-
-import com.example.nthucs.prototype.Utility.Event;
-
-public class Events {
-
+public class Event {
 }
