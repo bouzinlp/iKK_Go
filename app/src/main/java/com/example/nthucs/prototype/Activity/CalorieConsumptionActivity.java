ab2878fb39d80c0982e53af9f0a0fe430efa2f3e
NTHUCS <sirs2233@gmail.com>
2016-11-06 19:52:54 +0800

    add graph view

--- app/src/main/java/com/example/nthucs/prototype/Activity/CalorieConsumptionActivity.java
+++ app/src/main/java/com/example/nthucs/prototype/Activity/CalorieConsumptionActivity.java
@@ -8,6 +8,11 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.nthucs.prototype.R;
+import com.jjoe64.graphview.DefaultLabelFormatter;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.helper.StaticLabelsFormatter;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 /**
  * Created by user on 2016/8/2.
@@ -28,6 +33,9 @@ public class CalorieConsumptionActivity extends AppCompatActivity {
 
         // process back button
         processBackControllers();
+
+        // temporary test graph view
+        drawExampleCalorieChart();
     }
 
     @Override
@@ -57,5 +65,26 @@ public class CalorieConsumptionActivity extends AppCompatActivity {
                 overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
             }
         });
+    }
+
+    // example data for drawing calorie consumption chart
+    private void drawExampleCalorieChart() {
+        GraphView graphView = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{
+                new DataPoint(1, 2534), new DataPoint(2, 2671), new DataPoint(3, 2568), new DataPoint(4, 2498),
+                new DataPoint(5, 2520), new DataPoint(6, 2466), new DataPoint(7, 2386)
+        });
+
+        // activate horizontal scrolling
+        //graphView.getViewport().setScrollable(true);
+        // activate horizontal and vertical zooming and scrolling
+        //graphView.getViewport().setScalableY(true);
+
+        // use static labels for horizontal and vertical labels
+        StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graphView);
+        staticLabelsFormatter.setHorizontalLabels(new String[] {"Mon", "Tues", "Wed", "Thur", "Fri", "Sat", "Sun"});
+        graphView.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
+
+        graphView.addSeries(series);
     }
 }
--- app/src/main/java/com/example/nthucs/prototype/Activity/WeightChartActivity.java
+++ app/src/main/java/com/example/nthucs/prototype/Activity/WeightChartActivity.java
@@ -8,6 +8,10 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.nthucs.prototype.R;
+import com.jjoe64.graphview.DefaultLabelFormatter;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 /**
  * Created by user on 2016/8/2.
@@ -28,6 +32,9 @@ public class WeightChartActivity extends AppCompatActivity {
 
         // process back button
         processBackControllers();
+
+        // temporary test graph view
+        drawExampleWeightChart();
     }
 
     @Override
@@ -57,5 +64,42 @@ public class WeightChartActivity extends AppCompatActivity {
                 overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
             }
         });
+    }
+
+    // example data for drawing weight chart
+    private void drawExampleWeightChart() {
+        GraphView graphView = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{
+                new DataPoint(0, 0), new DataPoint(1, 68.5), new DataPoint(2, 68), new DataPoint(3, 67.8), new DataPoint(4, 67),
+                new DataPoint(5, 67.5), new DataPoint(6, 67.8), new DataPoint(7, 67.3), new DataPoint(8, 67), new DataPoint(9, 66.5),
+                new DataPoint(10, 66), new DataPoint(11, 65.5), new DataPoint(12, 66), new DataPoint(13, 65.4), new DataPoint(14, 65),
+                new DataPoint(15, 65.1), new DataPoint(16, 65.2), new DataPoint(17, 64.8), new DataPoint(18, 64), new DataPoint(19, 64.2),
+                new DataPoint(20, 64), new DataPoint(21, 63.6), new DataPoint(22, 63.4), new DataPoint(23, 63), new DataPoint(24, 62.7),
+                new DataPoint(25, 62.6), new DataPoint(26, 62.5), new DataPoint(27, 62.5), new DataPoint(28, 62.4), new DataPoint(29, 62.3),
+        });
+
+        // activate horizontal zooming and scrolling
+        //graphView.getViewport().setScalable(true);
+        // activate horizontal scrolling
+        graphView.getViewport().setScrollable(true);
+        // activate horizontal and vertical zooming and scrolling
+        graphView.getViewport().setScalableY(true);
+        // activate vertical scrolling
+        //graphView.getViewport().setScrollableY(true);
+
+        graphView.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() {
+            @Override
+            public String formatLabel(double value, boolean isValueY) {
+                if (isValueY) {
+                    // show normal y values
+                    return super.formatLabel(value, isValueY);
+                } else {
+                    // show currency for y values
+                    return super.formatLabel(value, isValueY) + " kg";
+                }
+            }
+        });
+
+        graphView.addSeries(series);
     }
 }
